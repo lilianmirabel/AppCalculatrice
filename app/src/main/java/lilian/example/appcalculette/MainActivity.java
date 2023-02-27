cba8b021d70ffe4b458e79922cddaa10c6f5c93c
lilian <lilianmirabel01120@gmail.com>
2023-02-27 09:21:15 +0100

    creation interface

--- app/src/main/java/lilian/example/appcalculette/MainActivity.java
+++ app/src/main/java/lilian/example/appcalculette/MainActivity.java
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.graphics.Color;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button monBouton;
+    private EditText bufferA;
+    private EditText bufferB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = new TextView(this);
-        textView.setTextColor(Color.RED);
-        textView.setText("Application Calculette");
-        setContentView(textView);
+       monBouton = (Button) findViewById(R.id.btCalculer);
+       bufferA = (EditText) findViewById(R.id.editNbA);
+       bufferB = (EditText) findViewById(R.id.editNbB);
     }
 }
